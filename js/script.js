2dfec367dcabbd9fdbcfb7d01be4f61eb4f8f528
Kennedy Musau <kennedy.musauu@gmail.com>
2023-11-03 19:47:33 +0300

    optimize site images

--- js/script.js
+++ js/script.js
@@ -49,8 +49,12 @@ const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
 
-    if (ent.isIntersecting === false) {
+    if (!ent.isIntersecting) {
       document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
     }
   },
   {
